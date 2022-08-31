2b32f60729c6b2db0a2fd63287ed49c4a4bbc97d
ElenaLatynina <latynina.elena.v@gmail.com>
2022-08-31 14:43:34 +0300

    Notifix

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,40 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import createMarkup from './createMarkup';
 
 const searchForm = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
+const galleryList = document.querySelector('.gallery');
+const loadMoreBTN = document.querySelector('.load-more');
+
+const searchImg = new AxiosRequestService();
+const gallery = new SimpleLightbox('.gallery a', { 
+    scrollZoom: false,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+ 
+searchForm.addEventListener('submt', onSearchImg);
+loadMoreBTN.addEventListener('click', onLoadMore);
+
+let totalHits = 0;
+
+async function onSearchImg(event) {
+    event.preventDefault();
+    clearMarkup();
+    const searchValue = event.currentTarget.elements.searchQuery.value.trim();
+
+    if (!searchValue) {
+        return
+    }
+
+    searchImg.query = searchValue;
+    searchImg.resetPage();
+
+    const images = await searchImg.onGetImage();
+
+    if (images.hits.length === 0) {
+        Notiflix.Notify.failure{ `Sorry, there are no images matching your search query. Please try again.` };
+        return;
+    }
+
+    totalHits = images.totalHits;
+    Notiflix.Notify.success(`Hooray! We found ${totalHits}totalHits images.`)
+}
